dcc91a0409870d0de344787c96aae1c9a45d48af
jvitela <j.vitel@gmail.com>
2025-11-25 16:18:42 +0100

    setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
